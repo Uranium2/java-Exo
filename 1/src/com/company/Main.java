065465f4f1bfc2b7f17d02ef64079e7f5580bc48
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2018-11-23 10:56:19 +0100

    Dynamic arrays

--- 1/src/com/company/Main.java
+++ 1/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,25 +9,25 @@ public class Main {
         email();
         arrayLoop();
     }
-public static void email()
-{
-    String email = "abc@esgi.fr";
 
-    int arobaseIndex = email.indexOf('@');
-    int dotIndex = email.indexOf('.');
+    public static void email() {
+        String email = "abc@esgi.fr";
 
-    String name = email.substring(0, arobaseIndex);
-    String domain = email.substring(arobaseIndex + 1, dotIndex);
-    String end = email.substring(dotIndex + 1);
+        int arobaseIndex = email.indexOf('@');
+        int dotIndex = email.indexOf('.');
 
-    System.out.println(name.toUpperCase());
-    System.out.println(domain.toUpperCase());
-    System.out.println(end.toUpperCase());
-}
+        String name = email.substring(0, arobaseIndex);
+        String domain = email.substring(arobaseIndex + 1, dotIndex);
+        String end = email.substring(dotIndex + 1);
 
-    public static void arrayLoop(){
-        double[]a = {10,20,30};
-        double[]b = {40,50,60};
+        System.out.println(name.toUpperCase());
+        System.out.println(domain.toUpperCase());
+        System.out.println(end.toUpperCase());
+    }
+
+    public static void arrayLoop() {
+        double[] a = {10, 20, 30};
+        double[] b = {40, 50, 60};
         System.out.print("Before Permutation: \na = ");
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
@@ -47,7 +49,7 @@ public static void email()
             System.out.print(b[i] + " ");
         }
 
-        // Initialisation d'un tableau statique: Bien
+        // Initialisation d'un tableau statique: Bien allouer l'espace mémoire pour chaque case
         StringBuilder[] sb = new StringBuilder[10];
 
         for (int i = 0; i < 10; i++) {
@@ -56,6 +58,13 @@ public static void email()
 
         for (int i = 0; i < 10; i++) {
             System.out.println("sb[i] = " + sb[i]);
+        }
+
+        // Tableau dynamique
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(Integer.toString(i));
         }
     }
 }
