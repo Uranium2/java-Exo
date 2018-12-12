6ec1b8dac5b483d386f50b95d8b1456f5a5d511f
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2018-12-12 17:03:38 +0100

    n cesar Left + right

--- ExoJava/src/com/company/Exo3.java
+++ ExoJava/src/com/company/Exo3.java
@@ -3,6 +3,8 @@ package com.company;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Exo3 {
     public static void main(String[] args) throws IOException {
         Scanner sc1 = new Scanner(System.in);
@@ -11,15 +13,16 @@ public class Exo3 {
         String str = sc2.nextLine();
 
         checkValidity(alpha, str);
+        nRight(alpha, str, 2);
+        nLeft(alpha, str, 2);
     }
 
-    public  static void checkValidity(String alpha, String str){
+    public static void checkValidity(String alpha, String str) {
         for (int i = 0; i < alpha.length(); i++) {
             int tmp = 0;
             for (int j = 0; j < str.length(); j++) {
                 if (alpha.charAt(i) == str.charAt(j)) {
                     tmp++;
-                    System.out.println("yes");
                     break;
                 }
             }
@@ -28,5 +31,34 @@ public class Exo3 {
                 break;
             }
         }
+    }
+
+    public static void nRight(String alpha, String str, int n) {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < alpha.length(); j++) {
+                if (str.charAt(i) == alpha.charAt(j))
+                {
+                    if (j + n < alpha.length())
+                        System.out.print(alpha.charAt(j + n));
+                    else
+                        System.out.print(alpha.charAt((j - alpha.length()) + n));
+                }
+            }
+        }
+        System.out.println();
+    }
+    public static void nLeft(String alpha, String str, int n) {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < alpha.length(); j++) {
+                if (str.charAt(i) == alpha.charAt(j))
+                {
+                    if (j - n >= 0)
+                        System.out.print(alpha.charAt(j - n));
+                    else
+                        System.out.print(alpha.charAt((j + alpha.length()) - n));
+                }
+            }
+        }
+        System.out.println();
     }
 }
