239004763409e4b91571688b692d36c26858737e
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2018-12-13 11:01:41 +0100

    Good try catch and not using throws

--- ExoJava/src/com/company/Exo2.java
+++ ExoJava/src/com/company/Exo2.java
@@ -1,19 +1,28 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Exo2 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Tavernier\\Documents\\Cours\\JAVA\\projets\\ExoJava\\src\\com\\company\\sample.txt"));
-        String line;
+    public static void main(String[] args) {
+        BufferedReader reader;
         ArrayList<Integer> results = new ArrayList<>();
-        while ((line = reader.readLine()) != null)
-        {
-            Integer score = Integer.parseInt(line.substring(line.indexOf(":") + 1));
-            results.add(score);
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\Tavernier\\Documents\\Cours\\JAVA\\projets\\ExoJava\\src\\com\\company\\sample.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Integer score = Integer.parseInt(line.substring(line.indexOf(":") + 1));
+                results.add(score);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot read line");
         }
         int res = sumAll(results);
         System.out.printf("Average score is: " + res);
@@ -27,14 +36,23 @@ public class Exo2 {
         return res;
     }
 
-    public  static ArrayList<String> getEmails() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Tavernier\\Documents\\Cours\\JAVA\\projets\\ExoJava\\src\\com\\company\\sample.txt"));
-        String line;
+    public static ArrayList<String> getEmails() {
+        BufferedReader reader ;
         ArrayList<String> emails = new ArrayList<>();
-        while ((line = reader.readLine()) != null)
-        {
-            String email = line.substring(0, line.indexOf(":"));
-            emails.add(email);
+        try {
+            reader = new BufferedReader(new FileReader("C:\\Users\\Tavernier\\Documents\\Cours\\JAVA\\projets\\ExoJava\\src\\com\\company\\sample.txt"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String email = line.substring(0, line.indexOf(":"));
+                emails.add(email);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot read line");
         }
         return emails;
     }
