ed87f047026d6e1351e087572928746fdc5ae1f7
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2019-01-31 09:56:18 +0100

    Employee

---
+++ ExoJava/src/com/tp1/Employee.java
@@ -0,0 +1,11 @@
+package com.tp1;
+
+public class Employee {
+    String name;
+    String surname;
+    public Employee(String name, String surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+}
