b8e31a524d85bad2f8205e9e51bbfccf1e34b5d1
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2019-05-09 11:17:13 +0200

    [Heritage] Company

--- ExoJava/src/com/tpHeritage/excercie3/Company.java
+++ ExoJava/src/com/tpHeritage/excercie3/Company.java
@@ -1,4 +1,54 @@
 package com.tpHeritage.excercie3;
 
-public class Company {
+public class Company extends Building {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmployeeCounter() {
+        return employeeCounter;
+    }
+
+    public void setEmployeeCounter(Integer employeeCounter) {
+        this.employeeCounter = employeeCounter;
+    }
+
+    public Double getAvg() {
+        return avg;
+    }
+
+    public void setAvg(Double avg) {
+        this.avg = avg;
+    }
+
+    Integer employeeCounter;
+    Double avg;
+
+    public Company(String owner, String address, Double area, String name, Integer employeeCounter, Double avg) {
+        super(owner, address, area);
+        this.name = name;
+        this.employeeCounter = employeeCounter;
+        this.avg = avg;
+    }
+
+    @Override
+    public Double tax() {
+        return 6.30 * getArea();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Address: " + getAddress() +
+                " Owner: " + getOwner() +
+                " Area of building: " + getArea() +
+                " Company name: " + getName() +
+                " Number of employees: " + getEmployeeCounter() +
+                " Average daily delivery: " + getAvg());
+    }
 }
