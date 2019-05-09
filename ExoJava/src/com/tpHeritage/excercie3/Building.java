ad2c4b914cde8313f3b8354d6d5ff9491192c27d
DESKTOP-8VHS1B3\Tavernier <antoine.tavernier@epita.fr>
2019-05-09 10:50:12 +0200

    [Heritage] Building class (a)

---
+++ ExoJava/src/com/tpHeritage/excercie3/Building.java
@@ -0,0 +1,46 @@
+package com.tpHeritage.excercie3;
+
+public class Building {
+    String owner;
+    String address;
+    Double area;
+
+    public Building(String owner, String address, Double area) {
+        this.owner = owner;
+        this.address = address;
+        this.area = area;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public Double tax() {
+        // switch on building type
+        return 0.0;
+    }
+
+    public void print() {
+        System.out.println(getAddress() + getOwner() + getArea());
+    }
+}
